5e244b752657d9e7da6ad61d09045fed2f888aeb
Alessandro Senese <senesealessandro@gmail.com>
2022-11-02 12:34:23 +0100

    fix AnimaAnimationControl

---
+++ addons/anima/visual-editor/editor/AnimaAddFrame.gd
@@ -0,0 +1,24 @@
+tool
+extends Control
+
+var _is_collapsed_mode := true
+
+signal add_frame
+signal add_event
+signal add_delay
+
+var _ratio: float = 1.0
+
+func _ready():
+	var final_width: float = 360
+
+	rect_min_size.x = final_width * _ratio
+
+func _on_Animation_pressed():
+	emit_signal("add_frame")
+
+func _on_Delay_pressed():
+	emit_signal("add_delay")
+
+func _on_Event_pressed():
+	emit_signal("add_event")
