f9ac33abb73acaf27a069e40935c723a337dfbe5
ceceppa <senesealessandro@gmail.com>
2022-11-19 17:18:32 +0000

    visual editor improvements

--- addons/anima/visual-editor/shared/AnimaButton.gd
+++ addons/anima/visual-editor/shared/AnimaButton.gd
@@ -20,9 +20,6 @@ enum STYLE {
 export (STYLE) var style = STYLE.PRIMARY setget set_button_style
 #export (float, 0.0, 1.0) var default_opacity := 1.0 setget set_default_opacity
 var default_opacity := 1.0 setget set_default_opacity
-var _zoom_on_hover = 1.05
-
-export (bool) var zoom_on_hover := true
 
 const COLORS := {
 	STYLE.PRIMARY: "#05445E",
@@ -30,24 +27,63 @@ const COLORS := {
 	STYLE.REMOVE: "#450003",
 }
 
+const PADDING := 24
+const BORDER_RADIUS := 4.0
+
 var _button_bg: Color = COLORS[0] setget _set_button_bg
 var _old_draw_mode
 var _force_default_zoom := true
+var _font: DynamicFont
+var _font_color: Color = get_color("font_color")
+var _text_position: Vector2
+var _text_size := Vector2.ZERO
+var _box_style := StyleBoxFlat.new()
 
 func _ready():
-#	$Inner.rect_min_size = rect_min_size
-#	$Inner.rect_size = rect_size
-#	$Inner.icon_align = icon_align
-	for property in PROPERTIES_TO_COPY:
-		$Inner.set(property, self[property])
+	_box_style.corner_radius_bottom_left = BORDER_RADIUS
+	_box_style.corner_radius_bottom_right = BORDER_RADIUS
+	_box_style.corner_radius_top_left= BORDER_RADIUS
+	_box_style.corner_radius_top_right = BORDER_RADIUS
 
+	_font = get_font("font")
+#	_box_style.border_width_bottom = 1
+#	_box_style.border_width_left = 1
+#	_box_style.border_width_right = 1
+#	_box_style.border_width_top = 1
+
+	_calculate_text_size()
 	_refresh_button(get_draw_mode())
 
-func _draw():
-	draw_rect(Rect2(Vector2.ZERO, rect_size), _button_bg, true)
+func _calculate_text_size() -> void:
+	_text_size = Vector2.ZERO
 
-	if not flat:
-		draw_rect(Rect2(Vector2.ZERO, rect_size), _button_bg.lightened(0.3), false, 1.0)
+	if _font == null:
+		_font = get_font("font")
+
+	for c in text:
+		var size := _font.get_char_size(c.to_ascii()[0])
+
+		_text_size.y = max(_text_size.y, size.y)
+		_text_size.x += size.x
+
+	_text_position = (rect_size - _text_size) / 2
+
+	if align == ALIGN_LEFT:
+		_text_position.x = PADDING
+	elif align == ALIGN_RIGHT:
+		_text_position.x = rect_size.x - PADDING - _text_size.x
+
+	_text_position.y = (rect_size.y / 2) + (_text_size.y / 2)
+
+func _draw():
+	if _font == null:
+		_font = get_font("font")
+
+	_box_style.bg_color = _button_bg
+	_box_style.border_color = _button_bg.lightened(0.3)
+
+	draw_style_box(_box_style, Rect2(Vector2.ZERO, rect_size))
+	draw_string(_font, _text_position, text, _font_color)
 
 func _input(_event):
 	var draw_mode = get_draw_mode()
@@ -62,18 +98,13 @@ func _refresh_button(draw_mode: int) -> void:
 		var color: Color = COLORS[style]
 		var final_color = color
 		var final_opacity := default_opacity
-		var final_zoom := 1.0
 
 		if draw_mode == STATE.HOVERED:
 			final_color = color.lightened(0.1)
 			final_opacity = 1.0
-			final_zoom = _zoom_on_hover
 		elif draw_mode == STATE.PRESSED:
 			final_color = color.darkened(0.2)
 			final_opacity = 1.0
-
-		if not zoom_on_hover:
-			final_zoom = 1.0
 
 		Anima.begin_single_shot(self) \
 			.with(
@@ -81,16 +112,11 @@ func _refresh_button(draw_mode: int) -> void:
 					to = {
 						_button_bg = final_color,
 						opacity = final_opacity,
-						scale = Vector2.ONE * final_zoom,
 						easing = ANIMA.EASING.EASE_IN_EXPO
 					}
 				}, 0.15)
 			) \
 			.play()
-
-func _set(property, value):
-	if PROPERTIES_TO_COPY.find(property) >= 0 and is_inside_tree():
-		$Inner.set(property, value)
 
 func set_button_style(new_style: int) -> void:
 	style = new_style
@@ -110,16 +136,15 @@ func _on_Button_button_down():
 func _on_Button_button_up():
 	_refresh_button(get_draw_mode())
 
-func set_text(new_text: String) -> void:
-	text = new_text
-
-	$Inner.set_text(new_text)
-
 func set_default_opacity(new_opacity: float) -> void:
 	default_opacity = new_opacity
 
 	modulate.a = default_opacity
 
-func _on_Inner_item_rect_changed():
-	rect_min_size = $Inner.rect_size
-	rect_size = $Inner.rect_size
+func _set(property, value):
+	if property == "text":
+		_calculate_text_size()
+
+func _on_Button_minimum_size_changed():
+	_calculate_text_size()
+	update()
