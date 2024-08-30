9ab119bf1b58805c3224b3dfdf104ef2cd99c1bf
Alessandro Senese <senesealessandro@gmail.com>
2024-08-30 12:39:38 +0100

    add test for issue #43

--- addons/anima/animations/zooming_exits/zoom_out_right.gd
+++ addons/anima/animations/zooming_exits/zoom_out_right.gd
@@ -2,11 +2,11 @@ var KEYFRAMES := {
   40: {
 	opacity = 1,
 	scale = Vector3(0.475, 0.475, 0.475),
-	"translate:x": 42,
+	"translate:x" = 42,
   },
-  to = {
+  "to": {
 	opacity = 0,
 	scale = Vector3(0.1, 0.1, 0.1),
-	"translate:x": ":size:x",
+	"translate:x" = ":size:x",
   }
 }
